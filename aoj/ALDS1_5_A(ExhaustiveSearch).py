4e609e9491ae5020cb2140137d54525d25367c25
Hideshi <info@htsubo.com>
2016-03-27 15:09:59 +0900

    ADD

---
+++ aoj/ALDS1_5_A(ExhaustiveSearch).py
@@ -0,0 +1,23 @@
+﻿# -*- Coding: utf-8 -*-
+import sys
+
+def solve(n, A, i, m):
+    if m == 0:
+        return 1
+    if i >= n:
+        return 0
+
+    result = solve(n, A, i+1, m) | solve(n, A, i+1, m-A[i])
+    return result
+
+
+n = int(input())
+A = list(map(int, input().split(" ")))
+q = int(input())
+M = list(map(int, input().split(" ")))
+
+for i in range(0,q):
+	if solve(n, A, 0, M[i]) == True:
+		print('yes')
+	else:
+		print('no')
---
+++ aoj/ALDS1_5_C_KochCurve.py
@@ -0,0 +1,42 @@
+﻿# -*- Coding: utf-8 -*-
+from  math import *
+
+cos60 = cos(radians(60))
+sin60 = sin(radians(60))
+
+def KochCurve(n, p1, p2):
+	if n == 0:
+		return;
+	
+	s = [0.0, 0.0]
+	u = [0.0, 0.0]
+	t = [0.0, 0.0]
+	
+	s[0] = (2.0*p1[0] + 1.0*p2[0])/3.0
+	s[1] = (2.0*p1[1] + 1.0*p2[1])/3.0
+	t[0] = (1.0*p1[0] + 2.0*p2[0])/3.0
+	t[1] = (1.0*p1[1] + 2.0*p2[1])/3.0
+	u[0] = (t[0]-s[0])*cos60 - (t[1]-s[1])*sin60 + s[0]
+	u[1] = (t[0]-s[0])*sin60 + (t[1]-s[1])*cos60 + s[1]
+	
+	KochCurve(n-1, p1, s)
+	print( "{x:.8f} {y:.8f}".format(x=s[0], y=s[1]) )
+	KochCurve(n-1, s, u)
+	print( "{x:.8f} {y:.8f}".format(x=u[0], y=u[1]) )
+	KochCurve(n-1, u, t)
+	print( "{x:.8f} {y:.8f}".format(x=t[0], y=t[1]) )
+	KochCurve(n-1, t, p2)
+	
+def main():
+
+	start = [0.0,0.0]
+	end   = [100.0,0.0]
+
+	n = int(input())
+	
+	print( "{x:.8f} {y:.8f}".format(x=start[0], y=start[1]) )
+	KochCurve(n, start, end)
+	print( "{x:.8f} {y:.8f}".format(x=end[0], y=end[1]) )
+	
+if __name__ == '__main__':
+	main()
